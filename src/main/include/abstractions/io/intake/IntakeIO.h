bc13fea5520b226c11a08649961e17ede163d793
DAMIRDIEDUKHOV <Damir23864262>
2026-07-01 12:12:33 -0500

    feat: IO for intake, finished just IO

---
+++ src/main/include/abstractions/io/intake/IntakeIO.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "units/current.h"
+#include "units/voltage.h"
+
+struct IntakeIOInputs{
+    double position{0.0};
+    double setpoint{0.0};
+    units::volt_t pivotPIDOut{0_V};
+    units::volt_t pivotFFOut{0_V};
+    units::volt_t intakeVolts{0_V};
+    units::ampere_t intakeCurrent{0_A};
+
+    bool pivotAtSetpoint{false};
+};
+
+class IntakeIO {
+    public:
+        virtual void UpdateInputs(IntakeIOInputs& inputs) = 0;
+        virtual void SetIntakeVolts(units::volt_t volts) = 0;
+        virtual void SetIntakeDown(bool down) = 0;
+        virtual void SetIntakeSetpoint(double rot) = 0;
+        virtual ~IntakeIO() = default;
+};
---
+++ src/main/include/subsystems/IntakeSubsystem.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "frc2/command/SubsystemBase.h"
+
+class IntakeSubsystem : public frc2::SubsystemBase {
+public:
+    
+private:
+
+};
+
+IntakeSubsystem::IntakeSubsystem(/* args */)
+{
+}
+
+IntakeSubsystem::~IntakeSubsystem()
+{
+}
