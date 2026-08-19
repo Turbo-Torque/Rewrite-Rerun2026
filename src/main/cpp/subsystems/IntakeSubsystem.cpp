5372d98c6deb84e1f1edddb9efa3ecc2d529d5a0
DAMIRDIEDUKHOV <Damir23864262>
2026-08-19 16:42:26 -0500

    feat: intial commit

--- src/main/cpp/subsystems/IntakeSubsystem.cpp
+++ src/main/cpp/subsystems/IntakeSubsystem.cpp
@@ -15,15 +15,16 @@ frc2::CommandPtr IntakeSubsystem::PivotAndRunIntakeCommand() {
     .AndThen(frc2::cmd::Run([this] {SetIntakeVoltage(IntakeConstants::kIntakeVolts);}, {this}))
     .FinallyDo([this] {
         SetIntakeVoltage(0_V);
-        SetIntakeSetpoint(IntakeConstants::kIntakeHalfway);
+        SetIntakeSetpoint(IntakeConstants::kIntakeUp);
     });
 }
+
 
 frc2::CommandPtr IntakeSubsystem::AgitateCommand() {
     return frc2::cmd::Run([this] {SetIntakeSetpoint(IntakeConstants::kIntakeAgitate);}, {this})
     .Until([this]() { return inputs.pivotAtSetpoint; })
     .FinallyDo([this] {
-        SetIntakeSetpoint(IntakeConstants::kIntakeHalfway);
+        SetIntakeSetpoint(IntakeConstants::kIntakeUp);
     });
 }
 
